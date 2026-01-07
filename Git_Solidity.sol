5cefb784e25d1b53a2b3535aa4bd00a684c83426
web3ghost <andrewalex622@gmail.com.com>
2026-01-06 18:48:25 -0600

    Este es mi tercer commit en Git desde Solidity

---
+++ Git_Solidity.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Storage {
+string public saludo = "Hola, Mundo & Git!";
+string public despedida = "Adiós, Mundo & Git!";
+uint public numero = 42;
+bool public estado = true;
+}
